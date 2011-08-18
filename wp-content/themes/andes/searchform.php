7611da6ba5451aa97ebc8aa5f622e18762e76873
santi93 <santivilla93@gmail.com>
2011-08-18 03:32:53 -0500

    Search-form with expanding effect and magnifying glass image

--- wp-content/themes/andes/searchform.php
+++ wp-content/themes/andes/searchform.php
@@ -4,6 +4,7 @@
 */
 ?>
 	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search'); ?>" />
-		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'twentyeleven' ); ?>" />
+		<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( '' ); ?>" />
+		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search' ); ?>" />
 	</form>
+
